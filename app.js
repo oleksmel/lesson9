2701ada143b083a448029abee5f3e2842cbe8913
oleksmel <milliontoday8@gmail.com>
2023-05-20 01:19:14 +0300

    change

---
+++ app.js
@@ -0,0 +1,8 @@
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn")
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(){
+    modal.style.display = "block";
+}
