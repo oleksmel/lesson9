77f48f272a7db949f5361eda66a1f8f4e1eb7a7c
oleksmel <milliontoday8@gmail.com>
2023-05-21 13:31:19 +0300

    changes

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ btn.onclick = function() {
     modal.style.display = "block";
 }
 
-span.onclick - function() {
-    modal.style.display = "none";
+span.onclick = function() {
+    modal.style.display = "none"
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+       modal.style.display = "none";
+    }
 }
